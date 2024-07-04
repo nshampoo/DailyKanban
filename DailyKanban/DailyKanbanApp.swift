b4ac1c0fa195736cddcc932ad944f1121b4cf8ae
Nick Shampoe <ShampooShoulders@gmail.com>
2024-07-04 12:36:25 -0400

    Initial Commit

---
+++ DailyKanban/DailyKanbanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyKanbanApp.swift
+//  DailyKanban
+//
+//  Created by Nicholas Shampoe on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyKanbanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
